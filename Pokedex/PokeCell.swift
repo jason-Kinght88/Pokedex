d432b3e14e751b124a5f97fb60c6bce3667a3859
vocal-studios <jasonknight551@gmail.com>
2017-07-12 14:17:22 +0800

    commit 2
    ui done and collection cell class created

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,28 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Amy on 12/7/17.
+//  Copyright © 2017 Vocal Studios. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+        
+    }
+    
+}
